9d692d132cd7d21b1a3c40dda738e627a7f74309
Truffie <trufanov.vl2013@gmail.com>
2024-07-23 23:04:08 +0300

    feat: add components into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import about from 'img/assets/about.png';
+import explorer from 'img/assets/explorer.png';
+
+import { Header } from '@/components/Header/Header';
+import { Footer } from '@/components/Footer/Footer';
+import { Form } from '@/components/Form/Form';
+import Section from '@/components/Section/Section';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <Header />
+      <main>
+        <Section
+          name="promo"
+          title="Explore and Travel"
+          content={
+            <>
+              <p>Holiday finder</p>
+              <Form />
+            </>
+          }
+          image={{
+            src: explorer,
+            width: 506,
+            height: 483,
+            alt: 'explorer',
+            quality: 100,
+          }}
+          isMain={true}
+          className="wrapper layout-rows"
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Section
+          name="about"
+          title="A new way to explore the world"
+          content={
+            <>
+              <p>
+                For decades travellers have reached for Lonely Planet books when
+                looking to plan and execute their perfect trip, but now, they
+                can also let Lonely Planet Experiences lead the way
+              </p>
+              <button className="colored">Learn more</button>
+            </>
+          }
+          image={{
+            src: about,
+            width: 646,
+            height: 524,
+            alt: 'about',
+            quality: 100,
+          }}
+          reverseLayout={true}
+          isLast={true}
+          className="wrapper layout-rows"
+        />
+      </main>
+      <Footer />
+    </>
   );
 }
